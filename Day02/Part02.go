bbee64af5c2a3b8092cf0ea3b317f7a9efe7dbf4
Mattia Oldani <mattiaoldani6@gmail.com>
2020-12-21 21:47:44 +0100

    Create Part02.go

---
+++ Day02/Part02.go
@@ -0,0 +1,85 @@
+package main
+
+import "fmt"
+import "os"
+import "bufio"
+import "strconv"
+
+func main () {
+
+	var a,b []int
+	var char []rune
+	var password, valid []string
+	var N int
+	a,b,char,password=ReadPassword()
+  	valid,N=Password(a,b,char,password)
+  	fmt.Println("Il numero di password valide é", N)
+
+}
+
+func ReadPassword () (a []int, b []int, char []rune, password []string) {
+
+	scanner:=bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		var line string=scanner.Text()
+		var n1,n2 int
+		n1,line=Extreme(line)
+		n2,line=Extreme(line)
+		a=append(a, n1)
+		b=append(b, n2)
+		var c rune
+		c,line=Char(line)
+		char=append(char, c)
+		password=append(password, line)
+	}
+	return
+
+}
+
+func Extreme (line string) (ext int, rest string) {
+
+	counter:=0
+	for _,c:=range line {
+		if counter==0 {
+			counter++
+		} else {
+			if c==' ' || c=='-' {
+				break
+			} else {
+				counter++
+				break
+			}
+		}
+	}
+	ext,_=strconv.Atoi(line[0:counter])
+	rest=line[counter+1:]
+	return
+
+}
+
+func Char (line string) (char rune, rest string) {
+	
+	char=rune(line[0])
+	rest=line[3:]
+	return
+	
+}
+
+func Password (a []int, b []int, char []rune, password []string) (valid []string, N int) {
+
+	for i,psw:=range password {
+        	if (rune(char[i])==rune(psw[a[i]-1]))!=(rune(char[i])==rune(psw[b[i]-1])) {
+        		if !two {
+                		valid=append(valid, psw)
+                		N++
+            		}
+        	} else {
+            		if two {
+                		valid=append(valid, psw)
+                		N++
+            		}
+        	}
+    	}
+    	return
+    
+}
